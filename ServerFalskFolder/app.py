96816adfed23d4e4115945c8f82914ab6313297f
AlessandroSica <alessandrosica500@gmail.com>
2022-06-21 17:12:13 +0200

    feat: now it is possible to exchange information with the server, through API. It is possible to POST, GET, DELETE, PUT

--- ServerFalskFolder/app.py
+++ ServerFalskFolder/app.py
@@ -2,5 +2,22 @@ from flask import Flask
 
 app = Flask(__name__)
 @app.route('/')
-def index():
-    return "hello world"
+def indexGET():
+    return "GET"
+
+@app.route('/', methods=["POST"])
+def POST():
+    print("post")
+    return "POST"
+
+@app.route('/', methods=["PUT"])
+def PUT():
+    return "PUT"
+
+@app.route('/', methods=["DELETE"])
+def DELETE():
+    return "DELETE"
+
+@app.route('/tests/:id')
+def test():
+    return "GET a new test"
