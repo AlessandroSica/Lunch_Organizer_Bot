c06784d9733aeb77cb326266dc19ecc7d8e7136e
AlessandroSica <alessandrosica500@gmail.com>
2022-06-21 10:54:06 +0200

    feat: creation of a bot that has a specific token and can send a message at a specific time on slack.

---
+++ Lunch_Bot.py
@@ -0,0 +1,13 @@
+import logging
+import time
+from slack_sdk.web import WebClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+token= os.getenv('TOKEN')
+client = WebClient(token)
+while(1):
+    if (time.strftime('%H:%M', time.localtime()) == ("10:51")):
+        response = client.chat_postMessage(channel="test-python-bot", text="Now it is 12:30. What do you want to eat for lunch?")
+        time.sleep(60)
