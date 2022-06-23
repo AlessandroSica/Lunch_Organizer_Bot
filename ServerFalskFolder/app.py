e37d85140764b1c153a2e2d8636e1dabaae3ea85
AlessandroSica <alessandrosica500@gmail.com>
2022-06-23 12:16:16 +0200

    feat: the bot now, when a request GET is send to the server, sends a message on office in slack

--- ServerFalskFolder/app.py
+++ ServerFalskFolder/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+import logging
+import time
+from slack_sdk.web import WebClient
+from slack_sdk.webhook import WebhookClient
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+token= os.getenv('TOKEN')
+client = WebClient(token)
 
 app = Flask(__name__)
-@app.route('/')
-def indexGET():
-    return "GET"
+#@app.route('/')
+#def indexGET():
+#    return "GET"
 
 @app.route('/', methods=["POST"])
 def POST():
@@ -17,6 +27,13 @@ def PUT():
 @app.route('/', methods=["DELETE"])
 def DELETE():
     return "DELETE"
+
+@app.route('/')
+def SendMessage():
+    if (time.strftime('%H:%M', time.localtime()) == ("12:00")):
+        response = client.chat_postMessage(channel="office", text="Avete già ordinato il pranzo? (🙂 = si, 😐 = no)")
+        time.sleep(60)
+    return "GET"
 
 @app.route('/tests/:id')
 def test():
