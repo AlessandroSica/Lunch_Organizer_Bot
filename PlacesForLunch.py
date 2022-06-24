049c71e9f85ebe42c944fe2c27981dda136c9336
AlessandroSica <alessandrosica500@gmail.com>
2022-06-24 15:47:13 +0200

    feat: now the bot can makes better suggestions
    feat: The bot can now send a single message on slack containing all the info related to three restaurant that it suggest.

--- PlacesForLunch.py
+++ PlacesForLunch.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from random import randint
 
-PlacesForLunchFile= pd.read_csv("PlacesLunch-Sheet1.csv")
+PlacesForLunchFile= pd.read_csv("LunchPlaces-info-Sheet1.csv")
 print(PlacesForLunchFile)
 print("")
 
@@ -15,15 +15,28 @@ print("")
 print("The suggestion of the day:")
 Suggestion=ListPlaces[randint(0, len(ListPlaces)-1)]
 prevSug=Suggestion
-print("-", Suggestion)
+#print(PlacesForLunchFile.iloc[Row])
+
+def PrintInfo():
+    for i in range(len(PlacesForLunchFile)):
+        if Suggestion==PlacesForLunchFile.loc[i].at["Name"]:
+            Row=i
+    print(PlacesForLunchFile.loc[Row].at["Name"])
+    print(PlacesForLunchFile.loc[Row].at["Rating"], PlacesForLunchFile.loc[Row].at["Stars"], PlacesForLunchFile.loc[Row].at["Reviews"])
+    print(PlacesForLunchFile.loc[Row].at["Description"])
+    print(PlacesForLunchFile.loc[Row].at["Vegan "], PlacesForLunchFile.loc[Row].at["Vegeterian"])
+    print(PlacesForLunchFile.loc[Row].at["Delivery"])
+    print(PlacesForLunchFile.loc[Row].at["Take-Away"])
+    print(PlacesForLunchFile.loc[Row].at["Distance"])
+    print(PlacesForLunchFile.loc[Row].at["Price range"])
+    print("")
+PrintInfo()
 
 for i in range(2):
     NotDifferent=True
     while(NotDifferent):
         Suggestion=ListPlaces[randint(0, len(ListPlaces)-1)]
         if prevSug!=Suggestion:
-            print("-", Suggestion)
+            PrintInfo()
             prevSug=Suggestion
             NotDifferent=False
-        else:
-            NotDifferent=True
--- app.py
+++ app.py
@@ -26,30 +26,49 @@ def PUT():
 def DELETE():
     return "DELETE"
 
-PlacesForLunchFile= pd.read_csv("PlacesLunch-Sheet1.csv")
-#print(PlacesForLunchFile)
+PlacesForLunchFile= pd.read_csv("LunchPlaces-info-Sheet1.csv")
 ListPlaces=[]
 for i in range(len(PlacesForLunchFile)):
     for j in range(PlacesForLunchFile.loc[i].at["Votes"]):
         ListPlaces+=[PlacesForLunchFile.loc[i].at["Name"]]
-#print(ListPlaces)
+
+def FormatSuggestion(Suggestion):
+    for i in range(len(PlacesForLunchFile)):
+        if Suggestion==PlacesForLunchFile.loc[i].at["Name"]:
+            return {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*{0}*\n *{1}* ​{2}​ {3}\n {4}\n {5}{6}\n {7}​\n {8}​\n {9}\n {10}\n".format(PlacesForLunchFile.loc[i].at["Name"], PlacesForLunchFile.loc[i].at["Rating"], PlacesForLunchFile.loc[i].at["Stars"], PlacesForLunchFile.loc[i].at["Reviews"], PlacesForLunchFile.loc[i].at["Description"], PlacesForLunchFile.loc[i].at["Vegan "], PlacesForLunchFile.loc[i].at["Vegeterian"], PlacesForLunchFile.loc[i].at["Delivery"], PlacesForLunchFile.loc[i].at["Take-Away"], PlacesForLunchFile.loc[i].at["Distance"], PlacesForLunchFile.loc[i].at["Price range"])
+                },
+                "accessory": {
+                    "type": "image",
+                    "image_url": PlacesForLunchFile.loc[i].at["Image"],
+                    "alt_text": "food image"
+                }
+            }
+
+def getsuggestion():
+    return []
+
 @app.route('/')
-def SendMessage():
-    if (time.strftime('%H:%M', time.localtime()) == ("11:30")):
-        response = client.chat_postMessage(channel="office", text="*Have you already decided where to lunch? (🙂 = Yes, 😐 = No)*")
-        response = client.chat_postMessage(channel="office", 
-        blocks=[{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "*Here there are the suggestion of the day:*"
-			}
-		},
-        {
-			"type": "divider"
-		}])
-        for i in range(3):
-            response = client.chat_postMessage(channel="office", text=(ListPlaces[randint(0, len(ListPlaces)-1)]), link_names="", icon_emoji="", )
+def SendSuggestionLunch():
+    blocks=[]
+
+    blocks.append({"type": "section","text": {"type": "mrkdwn", "text": "*Have you already decided where to lunch? (🙂 = Yes, 😐 = No)*\n *Here there are the suggestion of the day:*"}})
+    blocks.append({"type": "divider"})
+
+    Suggestions=getsuggestion()
+    
+    for suggestion in Suggestions:
+        blocks.append(FormatSuggestion(suggestion))
+
+    blocks.append({"type": "divider"})
+    blocks.append({"type": "section","text": {"type": "mrkdwn","text": "*Key*\n 🌱= vegan\n ​🥕​= vegetarian\n "}})
+    blocks.append({"type": "divider"})
+    
+    response = client.chat_postMessage(channel="test-python-bot", blocks=blocks)
+    
     return "GET"
 
 @app.route('/tests/:id')
