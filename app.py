267c46b4b05de748cd0c4c4127e4321c11e5dd34
AlessandroSica <alessandrosica500@gmail.com>
2022-06-29 16:15:40 +0200

    style: code style fixed

--- app.py
+++ app.py
@@ -7,40 +7,40 @@ from flask import Flask
 from slack_sdk.web import WebClient
 from slack_sdk.webhook import WebhookClient
 
-Token = os.getenv('TOKEN')
-client = WebClient(Token)
-App = Flask(__name__)
+token = os.getenv('TOKEN')
+client = WebClient(token)
+app = Flask(__name__)
 
-PlacesForLunchFile = pd.read_csv("LunchPlaces-info-Sheet9.csv")
-ListPlaces = []
-for i in range(len(PlacesForLunchFile)):
+places_for_lunch_file = pd.read_csv(os.getenv('FILE_LUNCH'))
+list_places = []
+for i in range(len(places_for_lunch_file)):
 
-    for j in range(PlacesForLunchFile.loc[i].at["Votes"]):
+    for j in range(places_for_lunch_file.loc[i].at["Votes"]):
 
-        ListPlaces += [PlacesForLunchFile.loc[i].at["Name"]]
+        list_places += [places_for_lunch_file.loc[i].at["Name"]]
 
-def FormatHeaders(Suggestion):
+def FormatHeaders(suggestion):
 
-    for i in range(len(PlacesForLunchFile)):
+    for i in range(len(places_for_lunch_file)):
 
-        if Suggestion == PlacesForLunchFile.loc[i].at["Name"]:
+        if suggestion == places_for_lunch_file.loc[i].at["Name"]:
 
             return {
 			    "type": "header",
 			    "text": {
 			    	"type": "plain_text",
 			    	"text": "{1} {0}".format(
-                        PlacesForLunchFile.loc[i].at["Name"], 
-                        PlacesForLunchFile.loc[i].at["Emoji"]
+                        places_for_lunch_file.loc[i].at["Name"], 
+                        places_for_lunch_file.loc[i].at["Emoji"]
                     )
 			    }
 		    }
 
-def FormatSuggestions(Suggestion):
+def FormatSuggestions(suggestion):
 
-    for i in range(len(PlacesForLunchFile)):
+    for i in range(len(places_for_lunch_file)):
 
-        if Suggestion == PlacesForLunchFile.loc[i].at["Name"]:
+        if suggestion == places_for_lunch_file.loc[i].at["Name"]:
 
             return {
                 "type": "section",
@@ -48,177 +48,198 @@ def FormatSuggestions(Suggestion):
                     "type": "mrkdwn",
                     "text": "{0}\n {1}{2}  -{3}​  -{4}​  -{5}\n {6}\n <{7}|Learn more...>\n"
                     .format(
-                        PlacesForLunchFile.loc[i].at["Description"], 
-                        PlacesForLunchFile.loc[i].at["Vegan "], 
-                        PlacesForLunchFile.loc[i].at["Vegeterian"], 
-                        PlacesForLunchFile.loc[i].at["Delivery"], 
-                        PlacesForLunchFile.loc[i].at["Take-Away"], 
-                        PlacesForLunchFile.loc[i].at["Distance"], 
-                        PlacesForLunchFile.loc[i].at["Price range"], 
-                        PlacesForLunchFile.loc[i].at["Tripadvisor"]
+                        places_for_lunch_file.loc[i].at["Description"], 
+                        places_for_lunch_file.loc[i].at["Vegan "], 
+                        places_for_lunch_file.loc[i].at["Vegeterian"], 
+                        places_for_lunch_file.loc[i].at["Delivery"], 
+                        places_for_lunch_file.loc[i].at["Take-Away"], 
+                        places_for_lunch_file.loc[i].at["Distance"], 
+                        places_for_lunch_file.loc[i].at["Price range"], 
+                        places_for_lunch_file.loc[i].at["Tripadvisor"]
                     ),
                 },
                 "accessory": {
                     "type": "image",
-                    "image_url": PlacesForLunchFile.loc[i].at["Image"],
+                    "image_url": places_for_lunch_file.loc[i].at["Image"],
                     "alt_text": "food image"
                 }
             }
 
 def getSuggestion():
-    Suggestion = ListPlaces[randint(0, len(ListPlaces) - 1)]
-    SuggestionList = [Suggestion]
+    suggestion = list_places[randint(0, len(list_places) - 1)]
+    suggestion_list = [suggestion]
 
     for i in range(2):
         NotDifferent = True
 
         while(NotDifferent):
-            Suggestion = ListPlaces[randint(0, len(ListPlaces) - 1)]
+            suggestion = list_places[randint(0, len(list_places) - 1)]
 
-            for j in (SuggestionList):
+            for j in (suggestion_list):
 
-                if j == Suggestion:
-                    Suggestion = SuggestionList[-1]
+                if j == suggestion:
+                    suggestion = suggestion_list[-1]
                     NotDifferent = True
                 else:
                     NotDifferent = False
 
-        SuggestionList += [Suggestion]
+        suggestion_list += [suggestion]
 
-    return SuggestionList
+    return suggestion_list
     
-def ChoseEmoji(Suggestion):
+def ChoseEmoji(suggestion):
 
-    for i in range(len(PlacesForLunchFile)):
+    for i in range(len(places_for_lunch_file)):
         
-        if Suggestion == PlacesForLunchFile.loc[i].at["Name"]:
+        if suggestion == places_for_lunch_file.loc[i].at["Name"]:
 
-            return PlacesForLunchFile.loc[i].at["Emoji"]
+            return places_for_lunch_file.loc[i].at["Emoji"]
 
-ChannelName= os.getenv('CHANNEL_NAME')
-ThreadToken=0
+channel_name= os.getenv('CHANNEL_NAME')
 
-@App.route('/')
+@app.route('/')
 def SendSuggestionLunch():
-    global ThreadToken
-    blocks1 = []
-    blocks2 = []
+    global thread_token
+    blocks_1 = []
+    blocks_2 = []
 
-    blocks1.append({
+    blocks_1.append({
         "type": "header",
         "text": {
             "type": "plain_text", 
             "text": "Have you already decided where to lunch?"
         }
     })
-    blocks1.append({
+    blocks_1.append({
         "type": "section",
         "text": {
             "type": "plain_text", 
             "text": ":sunglasses: = Yes, I have already decided\n :dizzy_face: = No, what do you offer me?"
         }
     })
-    blocks2.append({
+    blocks_2.append({
         "type": "header",
         "text": {
             "type": "plain_text", 
             "text": "Here there are the suggestion of the day:"
         }
     })
-    blocks2.append({"type": "divider"})
+    blocks_2.append({"type": "divider"})
 
-    Suggestions = getSuggestion()
+    suggestions = getSuggestion()
     
-    for suggestion in Suggestions:
+    for sug in suggestions:
 
-        blocks2.append(FormatHeaders(suggestion))
-        blocks2.append(FormatSuggestions(suggestion))
+        blocks_2.append(FormatHeaders(sug))
+        blocks_2.append(FormatSuggestions(sug))
         
-    Emoji1 = ChoseEmoji(Suggestions[0])
-    Emoji2 = ChoseEmoji(Suggestions[1])
-    Emoji3 = ChoseEmoji(Suggestions[2])
+    emoji1 = ChoseEmoji(suggestions[0])
+    emoji2 = ChoseEmoji(suggestions[1])
+    emoji3 = ChoseEmoji(suggestions[2])
 
-    blocks2.append({
+    blocks_2.append({
         "type": "section",
         "text": {
             "type": "mrkdwn",
             "text": "*Key*\n 🌱= vegan\n ​🥕​= vegetarian\n"
         }
     })
-    blocks2.append({"type": "divider"})
-    blocks2.append({
+    blocks_2.append({"type": "divider"})
+    blocks_2.append({
         "type": "header",
         "text": {
             "type": "plain_text", 
             "text": "Do you like any of them?\n React with the respective emoji"
         }
     })
-    blocks2.append({
+    blocks_2.append({
         "type": "header",
         "text": {
             "type": "plain_text", 
-            "text": "{0} = 1,  {1} = 2,  {2} = 3".format(Emoji1, Emoji2, Emoji3)
+            "text": "{0} = 1,  {1} = 2,  {2} = 3".format(emoji1, emoji2, emoji3)
         }
     })
-    blocks2.append({"type": "divider"})
+    blocks_2.append({"type": "divider"})
 
-    response = client.chat_postMessage(channel = ChannelName, blocks = blocks1)
-    ThreadToken = response["ts"]
-    response = client.chat_postMessage(channel = ChannelName, thread_ts = ThreadToken, blocks = blocks2)
+    response = client.chat_postMessage(channel = channel_name, blocks = blocks_1)
+    thread_token = response["ts"]
+    response = client.chat_postMessage(channel = channel_name, thread_ts = thread_token, blocks = blocks_2)
 
     return "GET"
 
-@App.route('/Thread-Message')
+@app.route('/Thread-Message')
 def ThreadMessage():
-    blocks3 = []
+    blocks_3 = []
 
-    blocks3.append({
+    blocks_3.append({
+        "type": "header",
+        "text": {
+            "type": "plain_text", 
+            "text": "Almost lunch time! Choose your place and go to grab food!"
+        }
+    })
+
+    blocks_3.append({
 		"type": "section",
 		"text": {
 			"type": "mrkdwn",
-			"text": "This is a section block with checkboxes."
+			"text": "If you are staying in the office, where do you want to lunch?"
 		},
 		"accessory": {
-			"type": "checkboxes",
+			"type": "radio_buttons",
 			"options": [
 				{
 					"text": {
 						"type": "mrkdwn",
-						"text": "*this is mrkdwn text*"
-					},
-					"description": {
-						"type": "mrkdwn",
-						"text": "*this is mrkdwn text*"
+						"text": "*rooftop*"
 					},
 					"value": "value-0"
 				},
 				{
 					"text": {
 						"type": "mrkdwn",
-						"text": "*this is mrkdwn text*"
-					},
-					"description": {
-						"type": "mrkdwn",
-						"text": "*this is mrkdwn text*"
+						"text": "*lounge terrace*"
 					},
 					"value": "value-1"
 				},
 				{
 					"text": {
 						"type": "mrkdwn",
-						"text": "*this is mrkdwn text*"
-					},
-					"description": {
-						"type": "mrkdwn",
-						"text": "*this is mrkdwn text*"
+						"text": "*lounge*"
 					},
 					"value": "value-2"
+				},
+                {
+					"text": {
+						"type": "mrkdwn",
+						"text": "*office 411 terrace*"
+					},
+					"value": "value-3"
+				},
+                {
+					"text": {
+						"type": "mrkdwn",
+						"text": "*kitchen area 3 floor*"
+					},
+					"value": "value-3"
+				},
+                {
+					"text": {
+						"type": "mrkdwn",
+						"text": "*relax area 2 floor*"
+					},
+					"value": "value-3"
 				}
 			],
 			"action_id": "checkboxes-action"
 		}
 	})
 
-    response = client.chat_postMessage(channel = ChannelName, thread_ts = ThreadToken, blocks = blocks3)
-
+    response = client.chat_postMessage(channel = channel_name, thread_ts = thread_token, blocks = blocks_3)
+    #print(str(result = client.conversations_history(
+    #    channel="C03M32EE1K2",
+    #    inclusive=True,
+    #    oldest=thread_token,
+    #    limit=1
+    #)))
     return "GET"
